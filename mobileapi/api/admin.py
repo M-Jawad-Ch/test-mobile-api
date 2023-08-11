61116dcbab8e2f77ede92f66d8937e8537b2ac38
jawad <mjawadch55@gmail.com>
2023-08-11 22:00:48 +0500

    Models Added

--- mobileapi/api/admin.py
+++ mobileapi/api/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Worker, Unit, Visit
+
+
+@admin.register(Worker)
+class _Worder(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
+@admin.register(Unit)
+class _Unit(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
+@admin.register(Visit)
+class _Visit(admin.ModelAdmin):
+    search_fields = ('unit__name', 'unit__worker__name')
--- mobileapi/api/models.py
+++ mobileapi/api/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Worker(models.Model):
+    name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Unit(models.Model):
+    name = models.CharField(max_length=255)
+    worker = models.ForeignKey(
+        Worker,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Visit(models.Model):
+    datetime = models.DateTimeField(auto_now_add=True)
+    unit = models.ForeignKey(Unit, on_delete=models.SET_NULL, null=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self) -> str:
+        return f'{self.datetime} ~ {self.latitude} {self.longitude}'
